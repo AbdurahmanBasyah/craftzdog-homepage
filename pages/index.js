093569b34120f00e336db304846b348e93d68d34
abduh <muhammad.abdurahman@ui.ac.id>
2022-07-13 00:04:20 +0700

    add loading motion

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ import {
   List,
   ListItem,
   Icon,
-  useColorModeValue
+  useColorModeValue,
+  chakra,
+  shouldForwardProp
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
@@ -26,11 +28,14 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
 import { isSuccess } from '../functions/api'
+import { MotionConfig, motion } from 'framer-motion'
+import useWindowSize from '../hooks/useWindowSize'
 const Home = () => {
   const [numOfAwards, setNumOfAwards] = useState(5);
   const [bios, setBios] = useState([])
   const [awards, setAwards] = useState([])
   const [experiences, setExperiences] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   const totalAwards = awards.length;
 
   const handleMoreAwards = () => {
@@ -56,7 +61,6 @@ const Home = () => {
       console.error(err)
     })
     axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/experiences`, {}).then((res) => {
-      console.log(res);
       if (isSuccess(res)) {
         setExperiences((res?.data?.data).reverse())
       }
@@ -65,174 +69,206 @@ const Home = () => {
     })
   }, [])
 
-  return (
-    <Layout>
-      <Container>
-        <Box
-          borderRadius="lg"
-          mb={6}
-          p={3}
-          textAlign="center"
-          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+  const { width, height } = useWindowSize()
+
+  const StyledDiv = chakra(motion.div, {
+    shouldForwardProp: prop => {
+      return shouldForwardProp(prop) || prop === 'transition'
+    }
+  })
+
+
+  if (isLoading) {
+    return <MotionConfig reducedMotion="user">
+      <Box zIndex="999999" position="absolute" top="50%" left="50%" transform="translate(-50%, -50%)">
+        <StyledDiv
+          animate={{
+            width: ['100px', '200px', "200px", "100px", "100px", '100px', '200px', "200px", "100px", "100px", '100px', '100px', "200px", "100px", `${width / 2}px`, `${2 * width}px`],
+            height: ['100px', '200px', "200px", "100px", "100px", '100px', '200px', "200px", "100px", "100px", '100px', '100px', "200px", "100px", `${height / 2}px`, `${2 * height}px`],
+            // scale: [1, 2, 2, 1, 1, 1, 2, 2, 1, 1, 1, 2, 2, 1, 50, 100],
+            rotate: [0, 0, 270, 270, 0, 0, 0, 270, 270, 0, 0, 0, 270, 270, 0, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%", "20%", "20%", "50%", "50%", "20%", "20%", "20%", "50%", "50%", "20%", "50%"]
+          }}
+          transition={{
+            duration: 6,
+            ease: "easeInOut",
+            times: [0.5, 0.4, 0.8, 0.5, 0.4, 0.8, 0.5, 0.4, 0.8, 0.9],
+            repeat: null
+          }}
+          bgColor="orange"
+          onAnimationComplete={() => { setIsLoading(false) }}
         >
-          Hello, Welcome to my personal web site
-        </Box>
-
-        <Box display={{ md: 'flex' }}>
-          <Box flexGrow={1}>
-            <Heading as="h2" variant="page-title">
-              M Abdurahman Basyah
-            </Heading>
-            <p>Developer / Learner / High Achiever</p>
-          </Box>
+        </StyledDiv>
+      </Box >
+    </MotionConfig >
+  } else {
+    return (
+      <Layout>
+        <Container>
           <Box
-            flexShrink={0}
-            mt={{ base: 4, md: 0 }}
-            ml={{ md: 6 }}
+            borderRadius="lg"
+            mb={6}
+            p={3}
             textAlign="center"
+            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
           >
-            <Image
-              borderColor="whiteAlpha.800"
-              borderWidth={2}
-              borderStyle="solid"
-              maxWidth="100px"
-              display="inline-block"
-              borderRadius="full"
-              src="/images/profile.jpg"
-              alt="Profile image"
-            />
+            Hello, Welcome to my personal web site
           </Box>
-        </Box>
 
-        <Section delay={0.2}>
-          <Heading as="h3" variant="section-title">
-            Summary
-          </Heading>
-          <Paragraph>
-            Abduh has an interest in
-            business and technology, therefore, he took the opportunity
-            to study at Information Systems, University of Indonesia. He loves coding, problem solving, and also music and food. In the future, he wants to be known as someone
-            which is useful to many people so that he always takes
-            opportunities so that he can continue to grow.
-            {/* <NextLink href="/works/inkdrop">
+          <Box display={{ md: 'flex' }}>
+            <Box flexGrow={1}>
+              <Heading as="h2" variant="page-title">
+                M Abdurahman Basyah
+              </Heading>
+              <p>Developer / Learner / High Achiever</p>
+            </Box>
+            <Box
+              flexShrink={0}
+              mt={{ base: 4, md: 0 }}
+              ml={{ md: 6 }}
+              textAlign="center"
+            >
+              <Image
+                borderColor="whiteAlpha.800"
+                borderWidth={2}
+                borderStyle="solid"
+                maxWidth="100px"
+                display="inline-block"
+                borderRadius="full"
+                src="/images/profile.jpg"
+                alt="Profile image"
+              />
+            </Box>
+          </Box>
+          <Section delay={0.2}>
+            <Heading as="h3" variant="section-title">
+              Summary
+            </Heading>
+            <Paragraph>
+              Abduh has an interest in
+              business and technology, therefore, he took the opportunity
+              to study at Information Systems, University of Indonesia. He loves coding, problem solving, and also music and food. In the future, he wants to be known as someone
+              which is useful to many people so that he always takes
+              opportunities so that he can continue to grow.
+              {/* <NextLink href="/works/inkdrop">
             <Link>Inkdrop</Link>
           </NextLink> */}
-          </Paragraph>
-          <Box align="center" my={4}>
-            <NextLink href="/works">
-              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                My portfolio
-              </Button>
-            </NextLink>
-          </Box>
-        </Section>
+            </Paragraph>
+            <Box align="center" my={4}>
+              <NextLink href="/works">
+                <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                  My portfolio
+                </Button>
+              </NextLink>
+            </Box>
+          </Section>
 
-        <Section delay={0.4}>
-          <Heading as="h3" variant="section-title">
-            Bio
-          </Heading>
-          {bios.map((bio, index) => {
-            return (
-
-              <BioSection key={index}>
-                <BioYear>{bio.year}</BioYear>
-                {bio.description}
-              </BioSection>
-            )
-          })}
-        </Section>
-
-        <Section delay={0.6}>
-          <Heading as="h3" variant="section-title">
-            I ♥
-          </Heading>
-          <Paragraph>
-            Music, Video Games, Rubik, Cards, Magic, Geometry, Coding, You
-          </Paragraph>
-        </Section>
-
-        <Section delay={0.8}>
-          <Heading as="h3" variant="section-title">
-            Awards
-          </Heading>
-          {awards.map((el, idx) => {
-            if (idx < numOfAwards) {
+          <Section delay={0.4}>
+            <Heading as="h3" variant="section-title">
+              Bio
+            </Heading>
+            {bios.map((bio, index) => {
               return (
-                <BioSection key={idx}>
-                  <BioYear>{el.year}</BioYear>
-                  {el.description}
+
+                <BioSection key={index}>
+                  <BioYear>{bio.year}</BioYear>
+                  {bio.description}
                 </BioSection>
               )
-            }
-          })}
-          <Box my={4}>
-            <Button colorScheme="teal" variant="ghost" onClick={() => handleMoreAwards()}>
-              {numOfAwards < totalAwards ? "Load more" : "Load less"}
-            </Button>
-          </Box>
+            })}
+          </Section>
 
-          <Box align="center" my={4}>
-            <NextLink href="/awards">
-              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                My Achievements
+          <Section delay={0.6}>
+            <Heading as="h3" variant="section-title">
+              I ♥
+            </Heading>
+            <Paragraph>
+              Music, Video Games, Rubik, Cards, Magic, Geometry, Coding, You
+            </Paragraph>
+          </Section>
+
+          <Section delay={0.8}>
+            <Heading as="h3" variant="section-title">
+              Awards
+            </Heading>
+            {awards.map((el, idx) => {
+              if (idx < numOfAwards) {
+                return (
+                  <BioSection key={idx}>
+                    <BioYear>{el.year}</BioYear>
+                    {el.description}
+                  </BioSection>
+                )
+              }
+            })}
+            <Box my={4}>
+              <Button colorScheme="teal" variant="ghost" onClick={() => handleMoreAwards()}>
+                {numOfAwards < totalAwards ? "Load more" : "Load less"}
               </Button>
-            </NextLink>
-          </Box>
-        </Section>
+            </Box>
 
-        <Section delay={1.0}>
-          <Heading as="h3" variant="section-title">
-            Experiences
-          </Heading>
-          {experiences.map((experience, index) => {
-            return (
-              <BioSection key={index}>
-                <BioYear>{experience.year}</BioYear>
-                {experience.description}
-              </BioSection>
-            )
-          })}
-        </Section>
+            <Box align="center" my={4}>
+              <NextLink href="/awards">
+                <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                  My Achievements
+                </Button>
+              </NextLink>
+            </Box>
+          </Section>
 
-        <Section delay={1.2}>
-          <Heading as="h3" variant="section-title">
-            Find me on the Internet
-          </Heading>
-          <List>
-            <ListItem>
-              <Link href="https://github.com/AbdurahmanBasyah" target="_blank">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<Icon as={IoLogoGithub} />}
-                >
-                  AbdurahmanBasyah
-                </Button>
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="mailto:abdurrahmanbasyah@gmail.com" target="_blank">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<Icon as={IoMail} />}
-                >
-                  abdurrahmanbasyah@gmail.com
-                </Button>
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://linkedin.com/in/abduh962" target="_blank">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<Icon as={IoLogoLinkedin} />}
-                >
-                  abduh962
-                </Button>
-              </Link>
-            </ListItem>
-            {/* <ListItem>
+          <Section delay={1.0}>
+            <Heading as="h3" variant="section-title">
+              Experiences
+            </Heading>
+            {experiences.map((experience, index) => {
+              return (
+                <BioSection key={index}>
+                  <BioYear>{experience.year}</BioYear>
+                  {experience.description}
+                </BioSection>
+              )
+            })}
+          </Section>
+
+          <Section delay={1.2}>
+            <Heading as="h3" variant="section-title">
+              Find me on the Internet
+            </Heading>
+            <List>
+              <ListItem>
+                <Link href="https://github.com/AbdurahmanBasyah" target="_blank">
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<Icon as={IoLogoGithub} />}
+                  >
+                    AbdurahmanBasyah
+                  </Button>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="mailto:abdurrahmanbasyah@gmail.com" target="_blank">
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<Icon as={IoMail} />}
+                  >
+                    abdurrahmanbasyah@gmail.com
+                  </Button>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link href="https://linkedin.com/in/abduh962" target="_blank">
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<Icon as={IoLogoLinkedin} />}
+                  >
+                    abduh962
+                  </Button>
+                </Link>
+              </ListItem>
+              {/* <ListItem>
               <Link href="https://twitter.com/yahhElahh" target="_blank">
                 <Button
                   variant="ghost"
@@ -243,18 +279,18 @@ const Home = () => {
                 </Button>
               </Link>
             </ListItem> */}
-            <ListItem>
-              <Link href="https://instagram.com/mabasyah" target="_blank">
-                <Button
-                  variant="ghost"
-                  colorScheme="teal"
-                  leftIcon={<Icon as={IoLogoInstagram} />}
-                >
-                  @mabasyah
-                </Button>
-              </Link>
-            </ListItem>
-            {/* <ListItem>
+              <ListItem>
+                <Link href="https://instagram.com/mabasyah" target="_blank">
+                  <Button
+                    variant="ghost"
+                    colorScheme="teal"
+                    leftIcon={<Icon as={IoLogoInstagram} />}
+                  >
+                    @mabasyah
+                  </Button>
+                </Link>
+              </ListItem>
+              {/* <ListItem>
             <Link href="https://discord.gg/QfsG5Kj" target="_blank">
               <Button
                 variant="ghost"
@@ -265,9 +301,9 @@ const Home = () => {
               </Button>
             </Link>
           </ListItem> */}
-          </List>
+            </List>
 
-          {/* <SimpleGrid columns={[1, 2, 2]} gap={6}>
+            {/* <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
             href="https://www.youtube.com/devaslife"
             title="Dev as Life"
@@ -283,10 +319,11 @@ const Home = () => {
             A Markdown note-taking app
           </GridItem>
         </SimpleGrid> */}
-        </Section>
-      </Container>
-    </Layout>
-  )
+          </Section>
+        </Container>
+      </Layout>
+    )
+  }
 }
 
 export default Home
